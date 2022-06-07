de675f01259bb3b888ec281fcee524f7349e69dd
alena safiullina <maskvakinka@mail.ru>
2022-06-07 19:18:25 +0300

    первый семинар готов

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int num, result;
+
+Console.Write( "введите целое число ");
+num = Convert.ToInt32(Console.ReadLine());
+
+result = num * num;
+
+Console.WriteLine("Результатик: " + result);
+*/
+
+Две другие задачи мне понятны, спасибо, но не стала их записывать.
