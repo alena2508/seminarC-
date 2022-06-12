e8a43febfb12ad035e89215759d69f22d8955e9a
alena safiullina <maskvakinka@mail.ru>
2022-06-12 14:26:14 +0300

    второй семинар

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,6 +1,5 @@
 ﻿/*
 int num, result;
-
 Console.Write( "введите целое число ");
 num = Convert.ToInt32(Console.ReadLine());
 result = num * num;
@@ -38,7 +37,6 @@ Console.WriteLine( "Большее число: " + max );
 
 /*
 int num, current;
-
 Console.WriteLine("введите целое число: ");
 num = Convert.ToInt32(Console.ReadLine());
 current = num / 2;
@@ -50,13 +48,58 @@ Console.WriteLine("Число чётное: ");
 }
 */
 
-int N, current; 
-
+/*int N, current; 
 Console.WriteLine("введите целое число: ");
 N = Convert.ToInt32(Console.ReadLine());
-
 while (N - 1 == 0)
 {
    N = N - 1;
-   Console.WriteLine(" N ");
+   Console.WriteLine(" N "); /* увы, не могу понять, как выводить каждое значение N
+}
+current = N / 2;
+current ++;
+if(current == 0) Console.WriteLine(" current, "); */
+
+
+
+
+/* ДЗ Семинар 2*/
+
+/* на вход трёхзначное число и на выходе показывает вторую цифру
+int CutNum()
+{
+    int num = new Random ().Next(100, 999);
+    Console.WriteLine($"выбранное число: {num}");
+    int sot = num / 10;
+    int result = sot % 10;
+    return result;
+}
+
+выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+int CutThreeNum ()
+{
+    int num, result;
+    Console.WriteLine("введите любое целое число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num < 100) Console.WriteLine(" третья цифра отсутствует ");
+    if (num > 100);
+    result = num / 10;
+    return result;
+}
+
+/*принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным*/
+
+int WeekDays ()
+{
+    int num;
+    Console.WriteLine("введите цифру дня недели (1-7) ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num >= 6)
+    {
+        return "True"; 
+    }
+    else
+    {
+        return "False";
+    }
 }
